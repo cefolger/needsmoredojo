468bfaa810cd719a0896cfbe09e2496b21cd189d
Christopher Folger <cefolger@gmail.com>
2013-06-16 19:03:52 -0400

    wrap in command module

--- src/com/chrisfolger/needsmoredojo/refactoring/ClassConverter.java
+++ src/com/chrisfolger/needsmoredojo/refactoring/ClassConverter.java
@@ -1,6 +1,10 @@
 package com.chrisfolger.needsmoredojo.refactoring;
 
+import com.chrisfolger.needsmoredojo.base.JSUtil;
 import com.intellij.lang.javascript.psi.*;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.psi.PsiElement;
 
 import java.util.List;
 
@@ -9,17 +13,43 @@ public class ClassConverter implements DeclareFinder.CompletionCallback
     @Override
     public void run(Object[] result)
     {
-        JSReturnStatement returnStatement = (JSReturnStatement) result[0];
+        final JSReturnStatement returnStatement = (JSReturnStatement) result[0];
         JSCallExpression declaration = (JSCallExpression) result[1];
-        List<JSExpressionStatement> methods = (List<JSExpressionStatement>) result[2];
+        final List<JSExpressionStatement> methods = (List<JSExpressionStatement>) result[2];
 
-        JSExpression[] mixins = ((JSArrayLiteralExpression) declaration.getArguments()[0]).getExpressions();
+        final JSExpression[] mixins = ((JSArrayLiteralExpression) declaration.getArguments()[0]).getExpressions();
 
-        doRefactor(mixins, methods, returnStatement);
+        CommandProcessor.getInstance().executeCommand(returnStatement.getProject(), new Runnable() {
+            @Override
+            public void run() {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        doRefactor(mixins, methods, returnStatement);
+                    }
+                });
+            }
+        },
+        "Convert util module to class module",
+        "Convert util module to class module");
     }
 
     public void doRefactor(JSExpression[] mixins, List<JSExpressionStatement> methods, JSReturnStatement originalReturnStatement)
     {
+        PsiElement parent = originalReturnStatement.getParent();
 
+        // build an array of mixins for the new declare statement
+        StringBuilder mixinArray = new StringBuilder();
+        for (JSExpression mixin : mixins) {
+            if (mixinArray.toString().equals("")) {
+                mixinArray.append(mixin.getText());
+            } else {
+                mixinArray.append(", " + mixin.getText());
+            }
+        }
+
+        // create the declare statement and add it before the return statement
+        String declareStatement = String.format("return declare([%s], {\n\n});", mixinArray.toString());
+        JSUtil.addStatementBeforeElement(parent, originalReturnStatement, declareStatement);
     }
 }
