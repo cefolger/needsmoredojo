45286991b05b1b39a48c2c601112e13ac1642dd2
Christopher Folger <cefolger@gmail.com>
2013-06-16 13:47:30 -0400

    finish test

--- tests/com/chrisfolger/needsmoredojo/base/TestDefineResolver.java
+++ tests/com/chrisfolger/needsmoredojo/base/TestDefineResolver.java
@@ -1,28 +1,12 @@
 package com.chrisfolger.needsmoredojo.base;
 
 import com.chrisfolger.needsmoredojo.testutil.MockJSArrayLiteralExpression;
-import com.chrisfolger.needsmoredojo.testutil.MockJSFunction;
 import com.chrisfolger.needsmoredojo.testutil.MockJSFunctionExpression;
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.Language;
 import com.intellij.lang.javascript.psi.*;
-import com.intellij.navigation.ItemPresentation;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Iconable;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.*;
-import com.intellij.psi.scope.PsiScopeProcessor;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.SearchScope;
-import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.psi.PsiElement;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,10 +29,11 @@ public class TestDefineResolver
     }
 
     @Test
-    public void foo()
+    public void testParametersAndDefinesAreParsedCorrectly()
     {
         JSRecursiveElementVisitor visitor = resolver.getDefineAndParametersVisitor(defines, parameters);
         visitor.visitJSCallExpression(getDojoAMDImportStatement(new String[] {"'dojo/_base/array'"}, new String[] { "array" }));
+        visitor.visitJSCallExpression(getDojoAMDImportStatement(new String[] {"'dijit/foo'"}, new String[] { "foo" }));
 
         assertEquals("'dojo/_base/array'", defines.get(0).getText());
     }
