c7e944909d03d3d20d0ae809a9f9fad8e0dfc6b7
Christopher Folger <cefolger@gmail.com>
2013-06-16 20:32:07 -0400

    add new import action

---
+++ src/com/chrisfolger/needsmoredojo/actions/AddNewImportAction.java
@@ -0,0 +1,13 @@
+package com.chrisfolger.needsmoredojo.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class AddNewImportAction extends AnAction
+{
+    @Override
+    public void actionPerformed(AnActionEvent e)
+    {
+
+    }
+}
