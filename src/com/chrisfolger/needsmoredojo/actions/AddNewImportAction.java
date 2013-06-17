fbc5ae6e8bbb39ee21c6ff69368a177d088a503c
Christopher Folger <cefolger@gmail.com>
2013-06-16 20:38:03 -0400

    add basic undo ability

--- src/com/chrisfolger/needsmoredojo/actions/AddNewImportAction.java
+++ src/com/chrisfolger/needsmoredojo/actions/AddNewImportAction.java
@@ -1,13 +1,34 @@
 package com.chrisfolger.needsmoredojo.actions;
 
+import com.chrisfolger.needsmoredojo.base.ImportCreator;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
 
 public class AddNewImportAction extends AnAction
 {
     @Override
     public void actionPerformed(AnActionEvent e)
     {
+        final PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        final String importModule = Messages.showInputDialog("", "Add new AMD import", null);
 
+        CommandProcessor.getInstance().executeCommand(psiFile.getProject(), new Runnable() {
+            @Override
+            public void run() {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        new ImportCreator().addImport(psiFile, importModule);
+                    }
+                });
+            }
+        },
+        "Add new AMD import",
+        "Add new AMD import");
     }
 }
---
+++ src/com/chrisfolger/needsmoredojo/base/ImportCreator.java
@@ -0,0 +1,11 @@
+package com.chrisfolger.needsmoredojo.base;
+
+import com.intellij.psi.PsiFile;
+
+public class ImportCreator
+{
+    public void addImport(PsiFile file, String module)
+    {
+
+    }
+}
