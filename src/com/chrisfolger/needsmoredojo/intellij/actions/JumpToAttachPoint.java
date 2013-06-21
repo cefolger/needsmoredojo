4fe22064451289254b60b27f22d3b47ca09920d6
Christopher Folger <cefolger@gmail.com>
2013-06-20 21:09:25 -0400

    add action

---
+++ src/com/chrisfolger/needsmoredojo/intellij/actions/JumpToAttachPoint.java
@@ -0,0 +1,11 @@
+package com.chrisfolger.needsmoredojo.intellij.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class JumpToAttachPoint extends AnAction
+{
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
