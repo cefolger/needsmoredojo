2b9b58b4a9ed5791feeb2b7c15468024820a0989
Christopher Folger <cefolger@gmail.com>
2013-06-24 20:14:23 -0400

    add custom listeners to get chosen directories

--- src/com/chrisfolger/needsmoredojo/intellij/configurable/DojoSettingsConfigurable.java
+++ src/com/chrisfolger/needsmoredojo/intellij/configurable/DojoSettingsConfigurable.java
@@ -8,9 +8,13 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.ComponentWithBrowseButton;
+import com.intellij.openapi.ui.TextComponentAccessor;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class DojoSettingsConfigurable implements Configurable {
     private JComponent myComponent;
@@ -18,6 +22,7 @@ public class DojoSettingsConfigurable implements Configurable {
     private JPanel myPanel;
     private TextFieldWithBrowseButton projectSourcesText;
     private TextFieldWithBrowseButton dojoSourcesText;
+    private Project project;
 
     public String getDisplayName() {
         return "Needs More Dojo";
@@ -27,16 +32,46 @@ public class DojoSettingsConfigurable implements Configurable {
         return true;
     }
 
+    private class ProjectSourcesChosen extends ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>
+    {
+        public ProjectSourcesChosen(@Nullable String title, @Nullable String description, FileChooserDescriptor fileChooserDescriptor) {
+            super(title, description, projectSourcesText, null, fileChooserDescriptor, TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            super.actionPerformed(e);
+
+            String projectSources = projectSourcesText.getText();
+        }
+    }
+
+    private class DojoSourcesChosen extends ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>
+    {
+        public DojoSourcesChosen(@Nullable String title, @Nullable String description, FileChooserDescriptor fileChooserDescriptor) {
+            super(title, description, dojoSourcesText, null, fileChooserDescriptor, TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            super.actionPerformed(e);
+
+            String dojoSources = dojoSourcesText.getText();
+
+        }
+    }
+
     public JComponent createComponent() {
         myComponent = (JComponent) myPanel;
         FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
 
-        projectSourcesText.addBrowseFolderListener("Project Sources", "Select the root of your project sources to support certain features of Needs More Dojo", null, descriptor);
-        dojoSourcesText.addBrowseFolderListener("Dojo Sources", "Select the root of the dojo library sources to support certain features of Needs More Dojo", null, descriptor);
+        projectSourcesText.addBrowseFolderListener(null, new ProjectSourcesChosen("Project Sources", "Select the root of your project's sources to support certain features of Needs More Dojo", descriptor));
+        dojoSourcesText.addBrowseFolderListener(null, new DojoSourcesChosen("Dojo Sources", "Select the root of the dojo library sources to support certain features of Needs More Dojo", descriptor));
 
         // don't know how else to get the current project???
         DataContext context = DataManager.getInstance().getDataContext();
         Project project = DataKeys.PROJECT.getData(context);
+        this.project = project;
 
         return myComponent;
     }
