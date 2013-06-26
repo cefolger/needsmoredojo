ecbc3c384fff729913f06b11a25b585164508a6c
Christopher Folger <cefolger@gmail.com>
2013-06-26 13:13:06 -0400

    show settings dialog if project and dojo sources are not setup

--- src/com/chrisfolger/needsmoredojo/intellij/components/DojoSettingsDetectionComponent.java
+++ src/com/chrisfolger/needsmoredojo/intellij/components/DojoSettingsDetectionComponent.java
@@ -4,9 +4,9 @@ import com.chrisfolger.needsmoredojo.core.settings.DojoSettings;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
@@ -56,12 +56,11 @@ public class DojoSettingsDetectionComponent implements ProjectComponent {
         new Notification("needsmoredojo", "Needs More Dojo", "It looks like you haven't set up dojo or project sources, which might make some features of Needs More Dojo work incorrectly. <a href=\"setup\">Set them up now...</a>", NotificationType.WARNING, new NotificationListener.Adapter() {
             @Override
             protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent hyperlinkEvent) {
-
+                ShowSettingsUtil.getInstance().showSettingsDialog(project, DojoSettings.class);
             }
         }).notify(project);
     }
 
     public void projectClosed() {
-        // called when project is being closed
     }
 }
