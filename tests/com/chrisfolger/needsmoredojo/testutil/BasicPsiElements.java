18c514e6f9b1ac8c00fa41300c8e28dd512e7a5a
Chris Folger <cefolger@gmail.com>
2013-07-14 19:22:42 -0400

    mock enhancements

--- tests/com/chrisfolger/needsmoredojo/testutil/BasicPsiElements.java
+++ tests/com/chrisfolger/needsmoredojo/testutil/BasicPsiElements.java
@@ -1,9 +1,12 @@
 package com.chrisfolger.needsmoredojo.testutil;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.lang.javascript.psi.JSElement;
 import com.intellij.lang.javascript.psi.JSExpression;
 import com.intellij.lang.javascript.psi.JSLiteralExpression;
+import com.intellij.lang.javascript.psi.impl.JSElementImpl;
 import com.intellij.lang.javascript.psi.impl.JSExpressionImpl;
+import com.intellij.psi.PsiElement;
 
 import static org.mockito.Mockito.mock;
 
@@ -12,6 +15,22 @@ public class BasicPsiElements
     public static JSLiteralExpression Null()
     {
         return new MockJSLiteralExpression("null");
+    }
+
+    public static JSElement elementFromText(final String text)
+    {
+        return new JSElementImpl(mock(ASTNode.class)) {
+            @Override
+            public String getText()
+            {
+                return text;
+            }
+        };
+    }
+
+    public static MockJSElement comma()
+    {
+        return new MockJSElement(",");
     }
 
     public static JSExpression expressionFromText(final String text)
---
+++ tests/com/chrisfolger/needsmoredojo/testutil/MockJSElement.java
@@ -0,0 +1,51 @@
+package com.chrisfolger.needsmoredojo.testutil;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.javascript.psi.JSElement;
+import com.intellij.lang.javascript.psi.impl.JSElementImpl;
+import com.intellij.psi.PsiElement;
+
+import static org.mockito.Mockito.mock;
+
+public class MockJSElement extends JSElementImpl
+{
+    private String text;
+    private MockJSElement nextSibling;
+    private PsiElement prevSibling;
+
+    public MockJSElement(String text) {
+        super(mock(ASTNode.class));
+
+        this.text = text;
+    }
+
+    @Override
+    public String getText()
+    {
+        return text;
+    }
+
+    @Override
+    public PsiElement getNextSibling()
+    {
+        return nextSibling;
+    }
+
+    @Override
+    public PsiElement getPrevSibling()
+    {
+        return prevSibling;
+    }
+
+    public void comesAfter(MockJSElement element)
+    {
+        element.prevSibling = this;
+        this.nextSibling = element;
+    }
+
+    public void comesBefore(MockJSElement element)
+    {
+        element.nextSibling = this;
+        this.prevSibling = element;
+    }
+}
