f3b0eb633f9531c050ebc5edfae78063f852a11a
Christopher Folger <cefolger@gmail.com>
2013-07-16 19:10:45 -0400

    add reorder action and temporary location + hotkey

---
+++ src/com/chrisfolger/needsmoredojo/intellij/actions/ReorderAMDImportAction.java
@@ -0,0 +1,12 @@
+package com.chrisfolger.needsmoredojo.intellij.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ReorderAMDImportAction extends JavaScriptAction
+{
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent)
+    {
+
+    }
+}
