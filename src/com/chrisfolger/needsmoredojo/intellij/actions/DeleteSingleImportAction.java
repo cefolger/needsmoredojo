bc86305442dc59ed0c7c16897309ac115bf75a24
Christopher Folger <cefolger@gmail.com>
2013-08-23 19:21:10 -0400

    initial xml entry and Action for deleting a single entry

---
+++ src/com/chrisfolger/needsmoredojo/intellij/actions/DeleteSingleImportAction.java
@@ -0,0 +1,11 @@
+package com.chrisfolger.needsmoredojo.intellij.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class DeleteSingleImportAction extends JavaScriptAction
+{
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent)
+    {
+    }
+}
