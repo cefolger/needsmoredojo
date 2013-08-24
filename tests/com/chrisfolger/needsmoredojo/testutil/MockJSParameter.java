6783452939e072261cc64ef20562a55dd8ec345e
Christopher Folger <cefolger@gmail.com>
2013-08-23 20:10:42 -0400

    update mock

--- tests/com/chrisfolger/needsmoredojo/testutil/MockJSParameter.java
+++ tests/com/chrisfolger/needsmoredojo/testutil/MockJSParameter.java
@@ -2,11 +2,15 @@ package com.chrisfolger.needsmoredojo.testutil;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.javascript.psi.impl.JSParameterImpl;
+import com.intellij.psi.PsiElement;
+import org.apache.commons.lang.NotImplementedException;
 
 import static org.mockito.Mockito.mock;
 
-public class MockJSParameter extends JSParameterImpl
+public class MockJSParameter extends JSParameterImpl implements MockJSElementInterface
 {
+    private MockJSElementInterface prevSibling;
+    private MockJSElementInterface nextSibling;
     String text = null;
 
     public MockJSParameter(String text) {
@@ -19,5 +23,40 @@ public class MockJSParameter extends JSParameterImpl
     public String getText()
     {
         return text;
+    }
+
+    @Override
+    public MockJSElementInterface comesBefore(MockJSElementInterface element) {
+        element.setPrevSibling(this);
+        this.nextSibling = element;
+
+        return this;
+    }
+
+    @Override
+    public void setPrevSibling(MockJSElementInterface element) {
+        this.prevSibling = element;
+    }
+
+    @Override
+    public void setNextSibling(MockJSElementInterface element) {
+        this.nextSibling = element;
+    }
+
+    @Override
+    public void setParent(MockJSElementInterface element) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public PsiElement getPrevSibling()
+    {
+        return this.prevSibling;
+    }
+
+    @Override
+    public PsiElement getNextSibling()
+    {
+        return this.nextSibling;
     }
 }
