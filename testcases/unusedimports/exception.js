2ee5a6ccec70168cb2b599b51e358eb6b3adcc4a
Christopher Folger <cefolger@gmail.com>
2013-09-07 11:19:25 -0400

    add issue102.zip test case

--- testcases/unusedimports/exception.js
+++
@@ -1,5 +0,0 @@
-
-/**
- * should not throw exception
- */
-define([], function() {});
--- testcases/unusedimports/removelast2.js
+++
@@ -1,8 +0,0 @@
-/*
-    the ] and , should not be removed
-*/
-define([
-    'dijit/_WidgetsInTemplateMixin','dijit/layout/ContentPane'], function(WidgetsInTemplateMixin, ContentPane) {
-    return declare([WidgetsInTemplateMixin], {
-    });
-});
