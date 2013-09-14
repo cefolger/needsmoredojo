f72812cb5eb862dd5cf4e2fe72bb4a6505ab4e11
Christopher Folger <cefolger@gmail.com>
2013-09-13 21:47:45 -0400

    add test cases to confirm new behavior

--- tests/com/chrisfolger/needsmoredojo/core/amd/psi/TestAMDPsiUtil.java
+++ tests/com/chrisfolger/needsmoredojo/core/amd/psi/TestAMDPsiUtil.java
@@ -1,22 +1,60 @@
 package com.chrisfolger.needsmoredojo.core.amd.psi;
 
+import com.chrisfolger.needsmoredojo.core.amd.importing.UnusedImportsRemover;
 import com.chrisfolger.needsmoredojo.testutil.BasicPsiElements;
 import com.chrisfolger.needsmoredojo.testutil.MockJSElementInterface;
 import com.chrisfolger.needsmoredojo.testutil.MockJSLiteralExpression;
 import com.chrisfolger.needsmoredojo.testutil.MockPsiComment;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestAMDPsiUtil
 {
-    @Test
-    public void foo()
+    private Set<PsiElement> removeList;
+
+    @Before
+    public void setup()
     {
-        new MockPsiComment("foo");
+        removeList = new HashSet<PsiElement>();
+    }
+
+    @Test
+    public void deleteDefineLiteral_normalCommentIsNotRemoved()
+    {
+        MockPsiComment comment = new MockPsiComment("/*foo*/");
+        MockJSLiteralExpression literal = new MockJSLiteralExpression("dijit/layout/ContentPane");
 
         BasicPsiElements.createChain(new MockJSElementInterface[]{
-                new MockJSLiteralExpression("dijit/layout/ContentPane"),
-
+                literal,
+                comment,
                 BasicPsiElements.comma()});
+
+        AMDPsiUtil.removeDefineLiteral(literal, removeList);
+
+        assertFalse(removeList.contains(comment));
+    }
+
+    @Test
+    public void deleteDefineLiteral_ignoreCommentIsRemoved()
+    {
+        MockPsiComment comment = new MockPsiComment(UnusedImportsRemover.IGNORE_COMMENT);
+        MockJSLiteralExpression literal = new MockJSLiteralExpression("dijit/layout/ContentPane");
+
+        BasicPsiElements.createChain(new MockJSElementInterface[]{
+                literal,
+                comment,
+                BasicPsiElements.comma()});
+
+        AMDPsiUtil.removeDefineLiteral(literal, removeList);
+
+        assertTrue(removeList.contains(comment));
     }
 }
