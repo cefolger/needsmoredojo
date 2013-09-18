48f6712af2a77f0b26a5e8f904ead1dfd7f50a2f
Christopher Folger <cefolger@gmail.com>
2013-09-18 12:22:34 -0400

    don't do refactoring if there are no project sources configured or if needs more dojo is disabled

--- src/com/chrisfolger/needsmoredojo/intellij/refactoring/MoveRefactoringProvider.java
+++ src/com/chrisfolger/needsmoredojo/intellij/refactoring/MoveRefactoringProvider.java
@@ -1,5 +1,9 @@
 package com.chrisfolger.needsmoredojo.intellij.refactoring;
 
+import com.chrisfolger.needsmoredojo.core.amd.filesystem.SourcesLocator;
+import com.chrisfolger.needsmoredojo.core.settings.DojoSettings;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.listeners.RefactoringElementListener;
@@ -28,6 +32,18 @@ public class MoveRefactoringProvider implements RefactoringElementListenerProvid
         if(!file.getText().contains("define"))
         {
             return null; // not a dojo module
+        }
+
+        if(!ServiceManager.getService(file.getProject(), DojoSettings.class).isNeedsMoreDojoEnabled())
+        {
+            return null; // don't want to refactor if we've disabled Needs More Dojo.
+        }
+
+        VirtualFile[] sources = SourcesLocator.getProjectSourceDirectories(file.getProject(), true);
+        if(sources.length == 0 || sources[0] == null)
+        {
+            // no project sources, so we can't really refactor
+            return null;
         }
 
         return new MoveRefactoringListener(file, file.getName());
